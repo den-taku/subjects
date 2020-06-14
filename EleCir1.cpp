f34366a8b9fa5f05db37a02488d1e8c3c3e3e566
Tada <sevensins0410s@icloud.com>
2020-06-14 13:33:41 +0900

    I/O debug (cin >>)
    practice

--- EleCir1.cpp
+++ EleCir1.cpp
@@ -1,1 +1,14 @@
 #include <iostream>
+using namespace std;
+
+int main(){
+  int n = 0;
+
+  cout << "How many times do you want to see?" << endl;
+  cout << ">> " << flush;
+  cin >> n;
+
+  cout << n << endl;
+
+  return 0;
+}  
